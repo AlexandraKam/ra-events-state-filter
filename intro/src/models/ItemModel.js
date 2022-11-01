3e144cb912d2b253882a818a181ee1c27c037e0f
Alexandra Kameneva <kameneva_fkp@mail.ru>
2022-11-01 19:01:07 +0100

    ДЗ 2 задача 1

--- intro/src/models/ItemModel.js
+++
@@ -1,8 +0,0 @@
-class ItemModel {
-    constructor(filters, selected, projects) {
-        this.filters = filters;
-        this.selected = selected;
-        this.projects = projects;
-    }
-}
-export default ItemModel;
